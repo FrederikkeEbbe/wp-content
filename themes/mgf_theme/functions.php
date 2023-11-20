b79f1a01eccb451428ac020aa69373fabe0a3794
claralacour <denlacour@gmail.com>
2023-11-20 14:31:34 +0100

    load css !

--- themes/mgf_theme/functions.php
+++ themes/mgf_theme/functions.php
@@ -3,4 +3,12 @@
         'main_menu' => 'Main Menu',
         'footer_menu' => 'Footer Menu'
     ));
+
+    function load_css(){
+        wp_register_style('main', get_stylesheet_directory_uri() . '/css/main.css', array(), false, 'all');
+        wp_enqueue_style('main');
+    }
+
+    add_action('wp_enqueue_scripts', 'load_css');
 ?>
+
--- themes/mgf_theme/header.php
+++ themes/mgf_theme/header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="<?php echo get_template_directory_uri()?>/style.css" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body>
