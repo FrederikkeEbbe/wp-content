35fa0082fa43cacba49d5b81c2b96f86c318aaf6
claralacour <denlacour@gmail.com>
2023-11-21 09:24:05 +0100

    rettelse

--- themes/mgf_theme/header.php
+++ themes/mgf_theme/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <?php /*her laver vi en theme hook, den sørger b.l.a for at vi får wp admin bar øverst på siden, så vi ved det er hooked korrekt*/ wp_head(); ?>
-</head>
+    <head>
+        <?php /*her laver vi en theme hook, den sørger b.l.a for at vi får wp admin bar øverst på siden, så vi ved det er hooked korrekt*/ wp_head(); ?>
+    </head>
 <body>
     
