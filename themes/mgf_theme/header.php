c73c0560b6db1677ccc107f8b658d43abac3496a
claralacour <denlacour@gmail.com>
2023-11-21 13:33:24 +0100

    linket til js

--- themes/mgf_theme/header.php
+++ themes/mgf_theme/header.php
@@ -5,6 +5,7 @@
         
         <!--Her linker jeg til stylesheet-->
         <link href="<?php echo get_template_directory_uri()?>/css/main.css" rel="stylesheet">
+        <script src="<?php echo get_template_directory_uri()?>/js/script.js" defer></script>
     </head>
 <body>
 <?php wp_nav_menu(); 
