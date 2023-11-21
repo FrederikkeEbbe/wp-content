ddcc19b398100580d91b09e6b61bd0ceb87c6e77
claralacour <denlacour@gmail.com>
2023-11-21 09:42:31 +0100

    js enqueue

--- themes/mgf_theme/functions.php
+++ themes/mgf_theme/functions.php
@@ -7,10 +7,17 @@
 
     /*her enqueuer vi vores css fil, så vi kan style */
     function load_css(){
-        wp_register_style('main', get_stylesheet_directory_uri() . '/css/main.css', array(), false, 'all');
+        wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
         wp_enqueue_style('main');
     }
 
     add_action('wp_enqueue_scripts', 'load_css');
+
+    /*her enqueuer vi vores js fil */
+    function load_js(){
+        wp_register_script('script', get_template_directory_uri() . '/js/script.js', array(), false, true);
+        wp_enqueue_script('script');
+    }
+    add_action('wp_enqueue_scripts', 'load_js');
 ?>
 
