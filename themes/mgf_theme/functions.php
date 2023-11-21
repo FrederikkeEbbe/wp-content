be8cb4e5e805e21711f18598f21388fe748b8796
FrederikkeEbbe <febbe8@gmail.com>
2023-11-21 10:36:16 +0100

    Ud kommentering

--- themes/mgf_theme/functions.php
+++ themes/mgf_theme/functions.php
@@ -5,7 +5,9 @@
         'footer_menu' => 'Footer Menu'
     ));
 
-    /*her enqueuer vi vores css fil, så vi kan style */
+    /*
+    Kommenteret ud. Vi behøver ikke denne del. Det bruges til hvis man skal lave child themes.
+    her enqueuer vi vores css fil, så vi kan style 
     function load_css(){
         wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
         wp_enqueue_style('main');
@@ -13,10 +15,11 @@
 
     add_action('wp_enqueue_scripts', 'load_css');
 
-    /*her enqueuer vi vores js fil */
+        her enqueuer vi vores js fil 
     function load_js(){
         wp_register_script('script', get_template_directory_uri() . '/js/script.js', array(), false, true);
     }
     add_action('wp_enqueue_scripts', 'load_js');
+    */
 
 
