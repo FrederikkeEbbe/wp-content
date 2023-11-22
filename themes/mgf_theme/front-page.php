67b4f7a2824b10e765515adcdb5981200522cfb6
FrederikkeEbbe <febbe8@gmail.com>
2023-11-22 12:27:51 +0100

    front page - post pis

--- themes/mgf_theme/front-page.php
+++ themes/mgf_theme/front-page.php
@@ -1,13 +1,35 @@
 
 <?php get_header(); /*her henter vi vores header fil(header.php), det er en wp funktion der er lavet i forvejen */ ?>
 
-<div>
-  <h1>  <?php the_title();?>  </h1>
-  
-  
-  <?php get_template_part('includes/section', 'content');?>
-
-
+<div class='hero-video'>
+  <?php //henter content på siden. Her mediefilen lagt ind i Home page
+  get_template_part('includes/section', 'content');
+  ?>
 </div>
+
+<section class='post-cat' >
+  <?php //følgende kode hjælper os med at få posts ind på siden.
+    $args = array(
+        'post_type' => 'post',  // Vi ønsker kun indlæg
+        'cat' => 7,             // Kategori ID er 7.
+        'posts_per_page' => -1, // Vis alle indlæg i kategorien
+    );
+
+    $custom_query = new WP_Query($args); //args = argumenter
+
+    if ($custom_query->have_posts()) :
+        while ($custom_query->have_posts()) : $custom_query->the_post();
+            ?>
+            <?php the_content(); 
+        endwhile;
+
+        // Ingen indlæg fundet i den specificerede kategori
+    endif;
+
+    // Gendan den oprindelige forespørgsel og nulstil postdata
+    wp_reset_postdata();
+  ?>
+</section> 
+
 
 <?php get_footer(); /*her henter vi vores footer fil(footer.php), det er en wp funktion der er lavet i forvejen */ ?>
