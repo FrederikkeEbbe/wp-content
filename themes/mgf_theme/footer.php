a96dbd67502a2359a88fcbd446458117a2a19d66
claralacour <denlacour@gmail.com>
2023-11-23 14:52:24 +0100

    slutprut

--- themes/mgf_theme/footer.php
+++ themes/mgf_theme/footer.php
@@ -1,4 +1,5 @@
 <footer>
+    <div class="footer-grid">
     <?php 
         wp_nav_menu(
             array(
@@ -10,7 +11,7 @@
     <?php //følgende kode hjælper os med at få posts ind på siden.
     $args = array(
         'post_type' => 'post',  // Vi ønsker kun indlæg
-        'cat' => 10,             // Kategori ID er 8.
+        'cat' => 10,             // Kategori ID er 10.
         'posts_per_page' => -1, // Vis alle indlæg i kategorien
     );
 
@@ -22,6 +23,43 @@
 
             <div class='footersection'>
             <?php the_content(); ?> </div>
+      <?php
+        endwhile;
+
+        // Ingen indlæg fundet i den specificerede kategori
+    endif;
+
+    // Gendan den oprindelige forespørgsel og nulstil postdata
+    wp_reset_postdata();
+  ?>
+  </div>
+  <?php //følgende kode hjælper os med at få posts ind på siden.
+    $args = array(
+        'post_type' => 'post',  // Vi ønsker kun indlæg
+        'cat' => 11,             // Kategori ID er 11.
+        'posts_per_page' => -1, // Vis alle indlæg i kategorien
+    );
+
+    $custom_query = new WP_Query($args); //args = argumenter
+ 
+    if ($custom_query->have_posts()) :
+        while ($custom_query->have_posts()) : $custom_query->the_post();
+            ?> 
+
+            <div class='somefooter'>
+                <?php the_content(); ?>
+                <div class="links">
+                    <a href="https://www.facebook.com/MunkebjergGymnastikforening">
+                        <img class="some" src="http://localhost/mgf/wp-content/uploads/2023/11/fb.png" alt="Facebook link">
+                    </a> 
+                    <a href="https://www.instagram.com/">
+                        <img class="some" src="http://localhost/mgf/wp-content/uploads/2023/11/insta.png" alt="Instagram link">
+                    </a> 
+                    <a href="https://www.tiktok.com/explore">
+                        <img class="some" src="http://localhost/mgf/wp-content/uploads/2023/11/tiktok.png" alt="Tiktok link">
+                    </a>
+                </div>
+            </div>
       <?php
         endwhile;
 
