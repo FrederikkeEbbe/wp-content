f89e40b0daf67393fd060357e3a35163da4b2296
claralacour <denlacour@gmail.com>
2023-11-23 10:48:16 +0100

    plugin

--- themes/mgf_theme/footer.php
+++ themes/mgf_theme/footer.php
@@ -3,13 +3,13 @@
     <?php echo get_post_meta(get_the_ID(), 'footer_section_1', true); ?>
   </div>
   <div class="til-medlemmer">
-    <?php echo get_post_meta(get_the_ID(), 'footer_section_1', true); ?>
+    <?php echo get_post_meta(get_the_ID(), 'footer_section_2', true); ?>
   </div>
   <div class="kontaktinfo">
-    <?php echo get_post_meta(get_the_ID(), 'footer_section_1', true); ?>
+    <?php echo get_post_meta(get_the_ID(), 'footer_section_3', true); ?>
   </div>
   <div class="til-instruktore">
-    <?php echo get_post_meta(get_the_ID(), 'footer_section_1', true); ?>
+    <?php echo get_post_meta(get_the_ID(), 'footer_section_4', true); ?>
   </div>
 </footer>
 <?php /*her laver en anden theme hook(se header for den anden)*/ wp_footer(); ?>
