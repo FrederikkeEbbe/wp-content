34c2dc8ec16466705f9f3dad10e65524c9dd3c55
claralacour <denlacour@gmail.com>
2023-11-27 10:29:01 +0100

    mangemangemange

--- themes/mgf_theme/template-hold.php
+++ themes/mgf_theme/template-hold.php
@@ -5,14 +5,13 @@ Template Name: Hold & tilmelding
 ?>
 
 <?php get_header(); /*her henter vi vores header fil(header.php), det er en wp funktion der er lavet i forvejen */ ?>
-<main>
-<div>
-  <h1>  <?php the_title();?>  </h1>
-  
-  
-  <?php get_template_part('includes/section', 'content');?>
-
-
+<div class="header-section">
+  <section class="padding">
+    <h1>  <?php the_title();?>  </h1>  
+    <?php get_template_part('includes/section', 'content');?>
+  </section>
 </div>
+<main>
+  
 </main>
 <?php get_footer(); /*her henter vi vores footer fil(footer.php), det er en wp funktion der er lavet i forvejen */ ?>
