405a8ff0cf8cd6d16308b7a44a8c07937bf7bea2
FrederikkeEbbe <febbe8@gmail.com>
2023-11-27 14:24:47 +0100

    Om os side

--- themes/mgf_theme/page-22.php
+++ themes/mgf_theme/page-22.php
@@ -7,41 +7,16 @@ Template Name: Om os
 <?php get_header(); /*her henter vi vores header fil(header.php), det er en wp funktion der er lavet i forvejen */ ?>
 
 
-<main>
+
 <section class='about-blue'>
   <div class='blue-post'>
     <?php get_template_part('includes/section', 'content');?>
   </div>
+  <img class="about-img" src="http://localhost/mgf/wp-content/uploads/2023/11/mgf-about.jpg" alt="Ungehold">
 </section>
 
-<section class='about-blue' >
-  <?php //følgende kode hjælper os med at få posts ind på siden.
-    $args = array(
-        'post_type' => 'post',  // Vi ønsker kun indlæg
-        'cat' => 13,             // Kategori ID er 8.
-        'posts_per_page' => -1, // Vis alle indlæg i kategorien
-    );
-
-    $custom_query = new WP_Query($args); //args = argumenter
- 
-    if ($custom_query->have_posts()) :
-        while ($custom_query->have_posts()) : $custom_query->the_post();
-            ?> 
-
-            <div class='blue-post'>
-
-            <?php the_content(); ?> </div>
-      <?php
-        endwhile;
-
-        // Ingen indlæg fundet i den specificerede kategori
-    endif;
-
-    // Gendan den oprindelige forespørgsel og nulstil postdata
-    wp_reset_postdata();
-  ?>
-</section> 
 <section class='about-reg' >
+  <img class="about-img" src="http://localhost/mgf/wp-content/uploads/2023/11/mgf-goals.jpg" alt="Ungehold">
   <?php //følgende kode hjælper os med at få posts ind på siden.
     $args = array(
         'post_type' => 'post',  // Vi ønsker kun indlæg
@@ -69,5 +44,36 @@ Template Name: Om os
   ?>
 </section> 
 
-</main>
+<section class='about-blue' >
+  <div class='blue-post'>
+  <?php //følgende kode hjælper os med at få posts ind på siden.
+    $args = array(
+        'post_type' => 'post',  // Vi ønsker kun indlæg
+        'cat' => 13,             // Kategori ID er 13.
+        'posts_per_page' => -1, // Vis alle indlæg i kategorien
+    );
+
+    $custom_query = new WP_Query($args); //args = argumenter
+ 
+    if ($custom_query->have_posts()) :
+        while ($custom_query->have_posts()) : $custom_query->the_post();
+            ?> 
+
+            <div class='blue-posts'>
+
+            <?php the_content(); ?> </div>
+      <?php
+        endwhile;
+
+        // Ingen indlæg fundet i den specificerede kategori
+    endif;
+
+    // Gendan den oprindelige forespørgsel og nulstil postdata
+    wp_reset_postdata();
+  ?>
+  </div>
+  <img class="about-img" src="http://localhost/mgf/wp-content/uploads/2023/11/mgf-forall.jpg" alt="Ungehold">
+</section> 
+
+
 <?php get_footer(); /*her henter vi vores footer fil(footer.php), det er en wp funktion der er lavet i forvejen */ ?>
