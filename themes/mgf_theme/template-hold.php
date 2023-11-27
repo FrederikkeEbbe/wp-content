eb35f903f31b3f5fb670a0241049b83b6d2d96e7
claralacour <denlacour@gmail.com>
2023-11-27 10:29:52 +0100

    holdoversigt begyndt

--- themes/mgf_theme/template-hold.php
+++ themes/mgf_theme/template-hold.php
@@ -11,7 +11,11 @@ Template Name: Hold & tilmelding
     <?php get_template_part('includes/section', 'content');?>
   </section>
 </div>
-<main>
-  
-</main>
+<div class ="teamtypes">
+  <a class='teamtypeslink' href="#"> 
+    <img class="teams-img" src="http://localhost/mgf/wp-content/uploads/2023/11/371521014_872576170988323_1780345655675662268_n.jpg" alt="Familie hold">
+    <h1>Familie</h1>
+  </a>
+                
+</div>
 <?php get_footer(); /*her henter vi vores footer fil(footer.php), det er en wp funktion der er lavet i forvejen */ ?>
