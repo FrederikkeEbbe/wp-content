1da5d929230d0ffd0de53d40b14d90a14d0b7173
FrederikkeEbbe <febbe8@gmail.com>
2023-11-29 11:04:04 +0100

    Bestyrelsen

---
+++ themes/mgf_theme/template-bestyrelse.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+Template Name: Bestyrelse
+*/
+?>
+
+<?php get_header(); /*her henter vi vores header fil(header.php), det er en wp funktion der er lavet i forvejen */ ?>
+
+        <?php get_template_part('includes/section', 'content');?>
+
+<main>
+</main>
+<?php get_footer(); /*her henter vi vores footer fil(footer.php), det er en wp funktion der er lavet i forvejen */ ?>
--- themes/mgf_theme/template-hold.php
+++ themes/mgf_theme/template-hold.php
@@ -43,8 +43,8 @@ Template Name: Hold & tilmelding
     <h1>Voksenhold</h1>
   </a>
   <a class="teamtypeslink" href="http://localhost/mgf/?page_id=104"> 
-    <img src="http://localhost/mgf/wp-content/uploads/2023/11/team-tg.jpg" alt="Team gym">
-    <h1>Teamgym</h1>
+    <img src="http://localhost/mgf/wp-content/uploads/2023/11/team-tg.jpg" alt="TeamGym">
+    <h1>TeamGym</h1>
   </a>
   <a class="teamtypeslink" href="http://localhost/mgf/?page_id=102"> 
     <img src="http://localhost/mgf/wp-content/uploads/2023/11/team-gp.jpg" alt="Grand prix">
