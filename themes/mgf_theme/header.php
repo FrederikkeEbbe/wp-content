0073149c3f93cb45680949c41b1d46ca3cc2b452
claralacour <denlacour@gmail.com>
2024-01-11 19:55:54 +0100

    comments

--- themes/mgf_theme/header.php
+++ themes/mgf_theme/header.php
@@ -4,6 +4,9 @@
         <?php /*her laver vi en theme hook, den sørger b.l.a for at vi får wp admin bar øverst på siden, så vi ved det er hooked korrekt*/ wp_head(); ?>
         
         <!--Her linker jeg til stylesheet-->
+        <!-- echo betyder vi outputter dataen-->
+        <!-- get_template_directory_uri - vi henter URI'en fra temaets directory, sørger for at hente det rigtige fra serveren, uanset om url strukturen er anerledes, så det virker på flere computere og wp sider-->
+
         <link href="<?php echo get_template_directory_uri()?>/css/main.css" rel="stylesheet">
         <script src="<?php echo get_template_directory_uri()?>/js/script.js" defer></script>
         <link rel="stylesheet" href="https://use.typekit.net/pdr3yon.css">
@@ -12,6 +15,7 @@
 <body>
 <header>
     <nav>
+        <!-- vi echoer vores home url ud i vores nav bar, det er logo som tilbage knap !-->
         <a href="<?php echo home_url()?>">
             <img class="logo" src="<?php echo get_home_url();?>/wp-content/uploads/2023/11/munkebjerg_logo.png" alt="Logo">
         </a>
