6f645b2de8b254a6d5a627304eb4b4b50e42095b
claralacour <denlacour@gmail.com>
2024-06-23 22:18:17 +0200

    news

--- themes/mgf_theme/front-page.php
+++ themes/mgf_theme/front-page.php
@@ -80,6 +80,35 @@
   ?>
 </section>
 
+<section class='home-news'>
+  <?php //følgende kode hjælper os med at få posts ind på siden.
+    $args = array(
+        'post_type' => 'post',  // Vi ønsker kun indlæg
+        'cat' => 16,             // Kategori ID er 8.
+        'posts_per_page' => -1, // Vis alle indlæg i kategorien
+    );
+
+    $custom_query = new WP_Query($args); //args = argumenter
+ 
+    if ($custom_query->have_posts()) :
+        while ($custom_query->have_posts()) : $custom_query->the_post();
+            ?> 
+
+            <div class='post-frontpage post-news'>
+
+            <?php the_content(); ?> 
+          </div>
+      <?php
+        endwhile;
+
+        // Ingen indlæg fundet i den specificerede kategori
+    endif;
+
+    // Gendan den oprindelige forespørgsel og nulstil postdata
+    wp_reset_postdata();
+  ?>
+</section>
+
 <section class='home-post' >
   <?php //følgende kode hjælper os med at få posts ind på siden.
     $args = array(
